e16ce884ac6e47f9b3863f25019acbe960ce23d2
PabloCarOr <44044102+PabloCarOr@users.noreply.github.com>
2023-10-04 00:09:49 -0600

    Update excel 2.py
    2 update para agregar la lectura del archivo excel xlsx

--- excel.py
+++ excel.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+import pip
+pip.main(["install", "openpyxl"])
+
 st.title('Subir base de datos de Excel')
 
 df = pd.read_excel('50000_Set_de_datos.xlsx')
