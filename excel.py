3b250739f21eb3c1367b12349b9da81cabbf3a1e
PabloCarOr <44044102+PabloCarOr@users.noreply.github.com>
2023-10-04 19:46:24 -0600

    Add files via upload

--- excel.py
+++ excel.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
 import os
+import pwd
 
-os.chown("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", int("adminuser"), -1)
+# Obtiene el UID del usuario 'adminuser'
+uid = pwd.getpwnam("adminuser").pw_uid
+
+# Cambia el propietario del directorio '/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile' al usuario 'adminuser'
+os.chown("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", uid, -1)
+
+# Cambia los permisos del directorio '/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile' a 777
 os.chmod("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", 777)
 
 import pip
