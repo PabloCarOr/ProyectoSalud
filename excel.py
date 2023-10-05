7d42665da891abc839aab43e2fb6601fb843a979
PabloCarOr <44044102+PabloCarOr@users.noreply.github.com>
2023-10-04 19:47:37 -0600

    Add files via upload

--- excel.py
+++ excel.py
@@ -1,22 +1,9 @@
 import streamlit as st
 import pandas as pd
-import os
-import pwd
-
-# Obtiene el UID del usuario 'adminuser'
-uid = pwd.getpwnam("adminuser").pw_uid
-
-# Cambia el propietario del directorio '/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile' al usuario 'adminuser'
-os.chown("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", uid, -1)
-
-# Cambia los permisos del directorio '/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile' a 777
-os.chmod("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", 777)
-
-import pip
-pip.main(["install", "openpyxl"])
-
-st.title('Subir base de datos de Excel')
+import openpyxl
 
 df = pd.read_excel('prueba.xlsx')
+
+st.title('Subir base de datos de Excel')
 
 st.write(df)
