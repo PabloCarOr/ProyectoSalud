1a9655aaad1e8cfa116674ee6b166daeb9e8ad1c
PabloCarOr <44044102+PabloCarOr@users.noreply.github.com>
2023-10-04 20:30:52 -0600

    Add files via upload

--- excel.py
+++ excel.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import openpyxl
 
-df = pd.read_excel('prueba.xlsx')
+df = pd.read_excel('50000_Set_de_datos.xlsx')
 
 st.title('Subir base de datos de Excel')
 
