a4ef13f531b8659983c071496012ce9bd0846cdf
PabloCarOr <44044102+PabloCarOr@users.noreply.github.com>
2023-10-04 19:38:46 -0600

    Add files via upload

--- excel.py
+++ excel.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import os
+
+os.chown("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", "adminuser")
+os.chmod("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", 777)
 
 import pip
 pip.main(["install", "openpyxl"])
