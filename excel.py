6bd27f931a81d9ad3b19508c50bc85ea0f52890e
PabloCarOr <44044102+PabloCarOr@users.noreply.github.com>
2023-10-04 21:58:06 -0600

    Add files via upload

--- excel.py
+++ excel.py
@@ -4,6 +4,14 @@ import openpyxl
 
 df = pd.read_excel('50000_Set_de_datos.xlsx')
 
-st.title('Subir base de datos de Excel')
+st.title('Proyecto Covid 19 / Equipo Pumas')
 
 st.write(df)
+
+# ----- Sidebar -----
+st.sidebar.header("Filtra aquí:")
+edad = st.sidebar.multiselect(
+    "Selecciona la ciudad:",
+    options=df["EDAD"].unique(),
+    default=df["EDAD"].unique()
+)
