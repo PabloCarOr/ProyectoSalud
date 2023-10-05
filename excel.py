3488e9f010b008f9b9388bed61e58e1e5bf99efc
PabloCarOr <44044102+PabloCarOr@users.noreply.github.com>
2023-10-04 19:45:01 -0600

    Add files via upload

--- excel.py
+++ excel.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 
-os.chown("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", "adminuser", -1)
+os.chown("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", int("adminuser"), -1)
 os.chmod("/home/adminuser/venv/lib/python3.9/site-packages/et_xmlfile", 777)
 
 import pip
